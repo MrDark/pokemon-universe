c0e0a3b5c32963e82cf39a2b80e5b358d76c23f0
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-05 21:40:44 +0000

    Window created

--- trunk/Client/main.go
+++ trunk/Client/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+    "fmt"
+    "os"
+    "sdl"
+)
+
+const (
+    WINDOW_WIDTH = 964
+    WINDOW_HEIGHT = 720
+)
+
 func main() {
-	println("I will become the PU client!")
+    //Initialize SDL
+    err := sdl.Init()
+    if err != "" {
+        fmt.Printf("Error in Init: %v", err)
+		return
+    }
+
+    //Create the window
+    err = sdl.CreateWindow("Pokemon Universe", WINDOW_WIDTH, WINDOW_HEIGHT)
+    if err != "" {
+        fmt.Printf("Error in CreateWindow: %v", err)
+		return
+    }
+
+	//Handle events
+    events := make(chan *sdl.SDLEvent)
+    go sdl.StartEventLoop(events)
+    for {
+        event := <- events
+		EventHandler(event)
+    }
+}
+
+func EventHandler(_event *sdl.SDLEvent) {
+	switch _event.Evtype {
+		case sdl.SDL_WINDOWEVENT:
+			HandleWindowEvent(_event.Window())
+	}
+}
+
+func HandleWindowEvent(_event *sdl.WindowEvent) {
+	switch _event.Event {
+		case sdl.SDL_WINDOWEVENT_CLOSE:
+			os.Exit(0)
+	}
 }
