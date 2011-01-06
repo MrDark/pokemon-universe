646ceee64b909d0b5d378bfa8429f6e576c152a1
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-06 00:18:45 +0000

    Forgot to make the license stuff a comment

--- trunk/Client/main.go
+++ trunk/Client/main.go
@@ -1,4 +1,4 @@
-Pokemon Universe MMORPG
+/*Pokemon Universe MMORPG
 Copyright (C) 2010 the Pokemon Universe Authors
 
 This program is free software; you can redistribute it and/or
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
 
 package main
 
