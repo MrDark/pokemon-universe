c317dbf2a320915102b478e063ec33056eaaf294
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-08 22:31:10 +0000

    Split up some engine code into engine file and added renderer creation code

--- Client/main.go
+++ Client/main.go
@@ -7,36 +7,31 @@ import (
     "time"
 )
  
-const (
-    WINDOW_WIDTH = 964
-    WINDOW_HEIGHT = 720
-)
- 
- 
 func main() {
     //Initialize SDL
     err := sdl.Init()
-    if err != "" {
+    if err != "" { 
         fmt.Printf("Error in Init: %v", err)
         return
     }
+ 
+	//Initialize the engine
+ 	InitEngine()
 
-    //Create the window
-    err = sdl.CreateWindow("Pokemon Universe", WINDOW_WIDTH, WINDOW_HEIGHT)
-    if err != "" {
-        fmt.Printf("Error in CreateWindow: %v", err)
-        return
-    }
-
-    //Handle events 
-    for { 
+	//Some test code
+    s := sdl.LoadImage("test.png")
+    texture := s.CreateTexture() 
+ 
+    //Handle events
+    for {  
         event, present := sdl.PollEvent()
 		if present {
 			EventHandler(event)		
 		}  
         sdl.RenderClear()
        
-        //draw
+		//Some more test code
+        texture.RenderCopy(sdl.Rect{0,0,int32(s.W),int32(s.H)}, sdl.Rect{0,0,int32(s.W),int32(s.H)})
    
         sdl.RenderPresent() 
         time.Sleep(10)
