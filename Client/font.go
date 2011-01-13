8c4caa49f032ca8ba7701c5fe933a2d6a4d03c8c
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-13 19:42:05 +0000

    Font is now colored at render-time, fontmap now keeps glyph textures of the different styles that are used (bold,italic,underlined) instead of for each used color

--- Client/font.go
+++ Client/font.go
@@ -8,7 +8,9 @@ import (
 type PU_Font struct {
 	font *sdl.Font
 	fontmap map[uint32]map[uint16]*PU_Image
-	color uint32
+	color *sdl.Color
+	size int
+	style uint32
 }
 
 func NewFont(_file string, _size int) *PU_Font {
@@ -17,7 +19,9 @@ func NewFont(_file string, _size int) *PU_Font {
 		fmt.Printf("Error loading Font: %v", sdl.GetError())
 	}
 	f := &PU_Font{font : sdlfont,
-				  fontmap : make(map[uint32]map[uint16]*PU_Image)}
+				  fontmap : make(map[uint32]map[uint16]*PU_Image),
+				  color : &sdl.Color{255,255,255,255},
+				  size : _size}
 	f.Build()
 	return f	
 }
@@ -27,30 +31,43 @@ func (f *PU_Font) Release() {
 }
 
 func (f *PU_Font) SetColor(_red uint8, _green uint8, _blue uint8) {
-	f.color = CreateColorKey(_red, _green, _blue)
+	f.color.R = _red
+	f.color.G = _green
+	f.color.B = _blue
 }
 
 func (f *PU_Font) SetStyle(_bold bool, _italic bool, _underline bool) {
-	f.font.SetStyle(_bold, _italic, _underline)
-	f.Build()
+	var b, i, u uint32 = 0, 0, 0
+	if _bold {
+		b = 1
+	}
+	if _italic {
+		i = 1
+	}
+	if _underline {
+		u = 1
+	}
+	
+	f.style = (b << 16) | (i << 8) | (u)
+	
+	_, present := f.fontmap[f.style]
+	if !present {
+		f.font.SetStyle(_bold, _italic, _underline)
+		f.Build()
+	}
 }
 
 func (f *PU_Font) Build() {
-	f.fontmap[f.color] = make(map[uint16]*PU_Image)
+	f.fontmap[f.style] = make(map[uint16]*PU_Image)
 	for c := 32; c <= 127; c++ {
-		surface := f.font.RenderText_Blended(fmt.Sprintf("%c",c), ColorKeyToSDL(f.color))
+		surface := f.font.RenderText_Blended(fmt.Sprintf("%c",c), ColorKeyToSDL(ColorCodeToKey(COLOR_WHITE)))
 		img := NewImageFromSurface(surface)
 		
-		f.fontmap[f.color][uint16(c)] = img
+		f.fontmap[f.style][uint16(c)] = img
 	}
 }
 
 func (f *PU_Font) DrawText(_text string, _x int, _y int) {
-	_, present := f.fontmap[f.color]
-	if !present {
-		f.Build()
-	}
-	
 	prev_char := -1
 	for c := 0; c < len(_text); c++ {
 		_, _, _, _, advance := f.font.GetMetrics(uint16(_text[c]))
@@ -62,8 +79,9 @@ func (f *PU_Font) DrawText(_text string, _x int, _y int) {
 			prev_char = int(_text[c])
 		}
 		
-		img := f.fontmap[f.color][uint16(_text[c])]
+		img := f.fontmap[f.style][uint16(_text[c])]
 		if img != nil {
+			img.SetColorMod(f.color.R, f.color.G, f.color.B)
 			img.Draw(_x, _y)
 			_x += advance
 		}
