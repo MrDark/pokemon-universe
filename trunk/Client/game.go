24620e034957a8201a5528dd5e4831d1289e5b46
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-17 16:56:11 +0000

    Removed test code, added procedures for loading game images, started on Game's Draw procedure

--- trunk/Client/game.go
+++ trunk/Client/game.go
@@ -24,43 +24,51 @@ import (
 	"strconv"
 )
 
+const (
+	GAMESTATE_UNLOADING = iota
+	GAMESTATE_LOADING
+	GAMESTATE_LOGIN
+	GAMESTATE_WORLD
+	GAMESTATE_BATTLE_INIT
+	GAMESTATE_BATTLE
+)
+
 type PU_Game struct {
+	state int
 	tileImageMap map[uint16]*PU_Image
+	guiImageMap map[uint16]*PU_Image
 }
 
 func NewGame() *PU_Game {
-	return &PU_Game{tileImageMap : make(map[uint16]*PU_Image)}
+	return &PU_Game{state : GAMESTATE_LOADING,
+					tileImageMap : make(map[uint16]*PU_Image),
+					guiImageMap : make(map[uint16]*PU_Image)}
 }
 
-func (g *PU_Game) LoadTileImages() {	
-	files, err := ioutil.ReadDir("data/tiles/")
-	if err != nil {
-		fmt.Printf("Couldn't open data/files/ directory. Error: %v\n", err.String())
-		return
-	}
-	
-	for i := 0; i < len(files); i++ {
-		g.LoadTileImage(files[i].Name)
-	}
-	
+func (g *PU_Game) SetState(_state int) {
+	g.state = _state
 }
 
-func (g *PU_Game) LoadTileImage(_file string) {
-	name := strings.Replace(_file, ".png", "", -1)
-	id, err := strconv.Atoi(name) 
-	if err != nil {
-		return
+func (g *PU_Game) Draw() {
+	switch g.state {
+		case GAMESTATE_LOADING:
+			g_engine.GetFont(FONT_PURITANBOLD_14).DrawText("Loading, please wait...", 10, 10)
+			
+		case GAMESTATE_LOGIN:
+			g.GetGuiImage(IMG_GUI_INTROBG).Draw(0, 0)
 	}
-	
-	surface := sdl.LoadImage("data/tiles/"+_file)
-	if surface == nil {
-		return
-	}
-	
-	image := NewImageFromSurface(surface)
-	g_engine.AddResource(image)
-	
-	g.tileImageMap[uint16(id)] = image
+}
+
+func (g *PU_Game) LoadFonts () {
+	g_engine.LoadFont(FONT_PURITANBOLD_14, "data/font/Puritan2Bold.otf", 14)
+}
+
+func (g* PU_Game) LoadTileImages() {
+	g.LoadGameImages("data/tiles/", g.tileImageMap)
+}
+
+func (g* PU_Game) LoadGuiImages() {
+	g.LoadGameImages("data/gui/", g.guiImageMap)
 }
 
 func (g *PU_Game) GetTileImage(_id uint16) *PU_Image {
@@ -68,4 +76,44 @@ func (g *PU_Game) GetTileImage(_id uint16) *PU_Image {
 		return image
 	}
 	return nil
+}
+
+func (g *PU_Game) GetGuiImage(_id uint16) *PU_Image {
+	if image, present := g.guiImageMap[_id]; present {
+		return image
+	}
+	return nil
+}
+
+func (g *PU_Game) LoadGameImages(_dir string, _map map[uint16]*PU_Image) {	
+	files, err := ioutil.ReadDir(_dir)
+	if err != nil {
+		fmt.Printf("Couldn't open directory: %v. Error: %v\n", _dir, err.String())
+		return
+	}
+	
+	for i := 0; i < len(files); i++ {
+		img, id := g.LoadGameImage(files[i].Name, _dir)
+		if img != nil {
+			_map[uint16(id)] = img
+		}
+	}
+}
+
+func (g *PU_Game) LoadGameImage(_file string, _dir string) (*PU_Image, int) {
+	name := strings.Replace(_file, ".png", "", -1)
+	id, err := strconv.Atoi(name) 
+	if err != nil {
+		return nil, 0
+	}
+	
+	surface := sdl.LoadImage(_dir+_file)
+	if surface == nil {
+		return nil, 0
+	}
+	
+	image := NewImageFromSurface(surface)
+	g_engine.AddResource(image)
+	
+	return image, id	
 }
