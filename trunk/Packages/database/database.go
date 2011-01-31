57485465af70e75acba7e6c1b5a4f654f402832e
mr_dark@darkweb.nl <mr_dark@darkweb.nl@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-31 22:04:35 +0000

    Initial version of our own generic database package

---
+++ trunk/Packages/database/database.go
@@ -0,0 +1,64 @@
+/*Pokemon Universe MMORPG
+Copyright (C) 2010 the Pokemon Universe Authors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+package db
+
+import (
+	"os"
+)
+
+type ClientInfo struct {
+	SQLHost	string
+	SQLUser	string
+	SQLPass	string
+	SQLDB	string
+}
+
+type Database interface {
+	// Connect to a database
+	Connect(_client ClientInfo) (err os.Error)
+	
+	// Close connection
+	Close()
+	
+	// Execute a query without storing the result
+	// Will return an error when failed
+	ExecuteQuery(_query string) (err os.Error)
+	
+	// Execute a query and store the output in a resultset
+	// Will return an error when failed
+	StoreQuery(_query string) (res ResultSet, err os.Error)
+	
+	// Free allocated memory
+	// Calls the Free() function in IResultSet
+	FreeResult(_result ResultSet)
+}
+
+type ResultSet interface {
+	GetData(_s string) interface{}
+	GetDataInt(_s string) int32
+	GetDataLong(_s string) int64
+	GetDataString(_s string) string
+	
+	// Next row
+	Next() bool
+	
+	// Total rows
+	Count() uint64
+	
+	// Clear resultset
+	Free()
+}
