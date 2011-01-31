5b52cb7b8c9c6b5180791ca4ad3dc7b5003b0de4
mr_dark@darkweb.nl <mr_dark@darkweb.nl@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-01-31 22:06:02 +0000

    Changed database connection from the slow GoMySQL to our own fast db package

--- trunk/Server/io/iomap_db.go
+++ trunk/Server/io/iomap_db.go
@@ -19,67 +19,68 @@ package main
 import (
 	"os"
 	pos "position"
-	"mysql"
+	"db"
 )
 
-type IOMapDB struct {
-
-}
+type IOMapDB struct{}
 
 func (io *IOMapDB) LoadMap(_map *Map) (err os.Error) {
-	var res *mysql.MySQLResult
-	var row map[string]interface{}
-	var	query string = "SELECT t.`x`, t.`y`, t.`z`, t.`idlocation`, t.`idmap`, t.`movement`, t.`idteleport`," +
-					   	" tl.`sprite`, tl.`layer`, tp.`x` AS `tp_x`, tp.`y` AS `tp_y`, tp.`z` AS `tp_z`" +
-					   	" FROM tile `t`" +
-						" INNER JOIN tile_layer `tl` ON tl.`idtile` = t.`idtile`" +
-						" LEFT JOIN teleport `tp` ON tp.`idteleport` = t.`idteleport`"
-	
-	if res, err = g_db.Query(query); err != nil {
+	var query string = "SELECT t.`x`, t.`y`, t.`z`, t.`idlocation`, t.`idmap`, t.`movement`, t.`idteleport`," +
+		" tl.`sprite`, tl.`layer`, tp.`x` AS `tp_x`, tp.`y` AS `tp_y`, tp.`z` AS `tp_z`" +
+		" FROM tile `t`" +
+		" INNER JOIN tile_layer `tl` ON tl.`idtile` = t.`idtile`" +
+		" LEFT JOIN teleport `tp` ON tp.`idteleport` = t.`idteleport`"
+
+	var result db.ResultSet
+	if result, err = g_db.StoreQuery(query); err != nil {
 		return
 	}
-	
+
+	g_logger.Printf(" - Loaded worldmap from database")
+
 	for {
-		if row = res.FetchMap(); row == nil {
-			break
-		}
-		
 		// DB vars
-		x			:=	row["x"].(int)
-		y			:=	row["y"].(int)
-		z			:=	row["z"].(int)
-		position	:=	pos.NewPositionFrom(x, y, z)
-		layer		:=	row["layer"].(int16)
-		sprite		:=	row["sprite"].(int32)
-		blocking	:=	row["movement"].(uint16)
-		tp_id		:=	row["idteleport"].(int32)
-		idlocation	:=	row["idlocation"].(int32)
-		
+		x 			:= result.GetDataInt("x")
+		y 			:= result.GetDataInt("y")
+		z 			:= result.GetDataInt("z")
+		position 	:= pos.NewPositionFrom(int(x), int(y), int(z))
+		layer		:= result.GetDataInt("layer")
+		sprite		:= result.GetDataInt("sprite")
+		blocking	:= result.GetDataInt("movement")
+		tp_id 		:= result.GetDataInt("idteleport")
+		idlocation	:= result.GetDataInt("idlocation")
+
 		tile, found := _map.GetTile(position.Hash())
 		if found == false {
 			tile = NewTile(position)
-			tile.Blocking = blocking
+			tile.Blocking = uint16(blocking)
 
 			// Get location
 			location, found := g_game.Locations.GetLocation(idlocation)
 			if found {
 				tile.Location = location
 			}
-			
+
 			// Teleport event
 			if tp_id > 0 {
-				tp_x	:= row["tp_x"].(int)
-				tp_y	:= row["tp_y"].(int)
-				tp_z	:= row["tp_z"].(int)
-				tp_pos	:= pos.NewPositionFrom(tp_x, tp_y, tp_z)
+				tp_x := result.GetDataInt("tp_x")
+				tp_y := result.GetDataInt("tp_y")
+				tp_z := result.GetDataInt("tp_z")
+				tp_pos := pos.NewPositionFrom(int(tp_x), int(tp_y), int(tp_z))
 				tile.Events = NewTeleport(tp_pos)
 			}
-			
+
 			_map.addTile(tile)
 		}
+
+		tile.AddLayer(int16(layer), int32(sprite))
 		
-		tile.AddLayer(layer, sprite)
-	}	
-						
+		if !result.Next() {
+			break
+		}
+	}
+	result.Free()
+
 	return
 }
+
