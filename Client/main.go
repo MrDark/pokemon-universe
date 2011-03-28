77e406eba5e99f3aa36154069324811c385bdafa
pu.urmel <pu.urmel@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-03-28 17:08:39 +0000

    Changed some constants to work with latest changes to the SDL wrapper

--- Client/main.go
+++ Client/main.go
@@ -144,6 +144,7 @@ func EventHandler(_event *sdl.SDLEvent) {
 func HandleWindowEvent(_event *sdl.WindowEvent) {
 	switch _event.Event {
 	case sdl.SDL_WINDOWEVENT_CLOSE:
+		println("hier")
 		g_running = false
 	}
 }
