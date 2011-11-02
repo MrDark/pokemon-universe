fec09308355fc578fe1d8c8a7dfddf4f304faf30
mr_dark@darkweb.nl <mr_dark@darkweb.nl@71effd0c-df8a-b9d7-22bd-8080055d6669>
2011-11-02 22:00:56 +0000

    More loading

--- Server/pokemon/pokemon.go
+++ Server/pokemon/pokemon.go
@@ -14,3 +14,9 @@ type Pokemon struct {
 	Abilities				map[int]*PokemonAbility
 	Forms					map[int]*PokemonForm
 }
+
+func NewPokemon() *Pokemon {
+	return &Pokemon{ Stats: make([]*PokemonStat, 6),
+					 Abilities: make(map[int]*PokemonAbility),
+					 Forms: make(map[int]*PokemonForm) }
+}
