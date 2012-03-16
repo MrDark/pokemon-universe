6bc24680e8b6228c235d1b4cf614e2aded88c8c2
ntilborg@gmail.com <ntilborg@gmail.com@71effd0c-df8a-b9d7-22bd-8080055d6669>
2012-03-16 14:40:45 +0000

    Expanded NPC handler with map position parameters, Split EditNpc to EditNpcPosition and EditNpcAppearance

--- trunk/Go/src/orgasm/npc.go
+++ trunk/Go/src/orgasm/npc.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	puh "puhelper"
-//	pos "putools/pos"
+	pos "putools/pos"
 )
 
 // Simple NPC creature, for test purposes for the editor
@@ -15,6 +15,9 @@ type Npc struct {
   	Upper 	int
 	Lower	int
   	Feet 	int
+  	X		int
+  	Y		int
+  	Z		int
 }
 
 type NpcList struct {
@@ -26,7 +29,7 @@ func NewNpcList() *NpcList{
 }
 
 func (m *NpcList) LoadNpcList() (succeed bool, error string) {
-	var query string = "SELECT npc.idnpc, npc.name, npc_outfit.head, npc_outfit.nek, npc_outfit.upper, npc_outfit.lower, npc_outfit.feet FROM npc INNER JOIN npc_outfit ON npc.idnpc = npc_outfit.idnpc ORDER BY npc.idnpc"
+	var query string = "SELECT npc.idnpc, npc.name, npc_outfit.head, npc_outfit.nek, npc_outfit.upper, npc_outfit.lower, npc_outfit.feet, npc.position FROM npc INNER JOIN npc_outfit ON npc.idnpc = npc_outfit.idnpc ORDER BY npc.idnpc"
 		
 	result, err := puh.DBQuerySelect(query)
 	if err != nil {
@@ -48,9 +51,14 @@ func (m *NpcList) LoadNpcList() (succeed bool, error string) {
 		upper := puh.DBGetInt(row[4])
 		lower := puh.DBGetInt(row[5])
 		feet := puh.DBGetInt(row[6])
+		positionHash := puh.DBGetInt64(row[7])
 		
+		position := pos.NewPositionFromHash(positionHash)
+		x := position.X
+		y := position.Y
+		z := position.Z
 		
-		m.AddNpc(idNpc, nameNpc, head, nek, upper, lower, feet )
+		m.AddNpc(idNpc, nameNpc, head, nek, upper, lower, feet, x, y, z )
 	}
 	
 	return true, ""
@@ -60,23 +68,26 @@ func (m *NpcList) GetNumNpcs() int {
 	return len(m.Npcs)
 }
 
-func (m *NpcList) AddNpc(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int) {
+func (m *NpcList) AddNpc(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int, _x int, _y int, _z int) {
 	npc := &Npc { Id: _npcId,
 				  Name: _npcName,
 				  Head: _head,
 				  Nek: _nek,
 				  Upper: _upper,
 				  Lower: _lower,
-				  Feet: _feet }
+				  Feet: _feet, 
+				  X: _x,
+				  Y: _y,
+				  Z: _z }
 	
 	m.Npcs[_npcId] = npc
 }
 
-func (m *NpcList) UpdateNpc(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int) {
+func (m *NpcList) UpdateNpcAppearance(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int) {
 	// Get Npc from list
 	npc, found := m.Npcs[_npcId]
 	if !found {
-		m.AddNpc(_npcId, _npcName, _head, _nek, _upper, _lower, _feet)
+		m.AddNpc(_npcId, _npcName, _head, _nek, _upper, _lower, _feet, 0, 0, 0)
 	} else {
 		npc.Name = _npcName
 		npc.Head = _head
@@ -85,4 +96,14 @@ func (m *NpcList) UpdateNpc(_npcId int, _npcName string, _head int, _nek int, _u
 		npc.Lower = _lower
 		npc.Feet = _feet
 	}
+}
+
+func (m *NpcList) UpdateNpcPosition(_npcId int, _x int, _y int, _z int) {
+	// Get Npc from list
+	npc, found := m.Npcs[_npcId]
+	if found {
+		npc.X = _x
+		npc.Y = _y
+		npc.Z = _z
+	} 
 }
