644e9f1c18cd43eb236f88e937ec17041e065197
mr_dark@darkweb.nl <mr_dark@darkweb.nl@71effd0c-df8a-b9d7-22bd-8080055d6669>
2012-04-05 14:37:51 +0000

    [SERVER] Base time service for calculating in-game time and weather

--- trunk/Go/src/puserver/const.go
+++ trunk/Go/src/puserver/const.go
@@ -61,6 +61,34 @@ const (
 )
 
 const (
+	WEATHER_NORMAL	int = iota
+	WEATHER_RAIN
+	WEATHER_HAIL
+	WEATHER_SANDSTORM
+	WEATHER_FOG
+)
+
+const (
+	SUNDAY int = iota
+	MONDAY
+	TUESDAY
+	WEDNESDAY
+	THURSDAY
+	FRIDAY
+	SATURDAY
+)
+
+var Days = [...]string{
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+}
+
+const (
 	CTYPE_CREATURE = 0
 	CTYPE_NPC      = 1
 	CTYPE_PLAYER   = 2
