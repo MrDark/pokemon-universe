faf00581635612cd64c4566b9085d72069d79988
ntilborg@gmail.com <ntilborg@gmail.com@71effd0c-df8a-b9d7-22bd-8080055d6669>
2012-04-16 19:40:38 +0000

    Implemented storage of NPC Pokemon and send NPC specific Pokemon to the client when the client retrieves them

--- trunk/Go/src/orgasm/npc.go
+++ trunk/Go/src/orgasm/npc.go
@@ -6,9 +6,7 @@ import (
 	pos "putools/pos"
 )
 
-// Simple NPC creature, for test purposes for the editor
 type Npc struct {
-	Id		int
   	Name 	string
   	Head 	int
   	Nek 	int
@@ -16,6 +14,19 @@ type Npc struct {
 	Lower	int
   	Feet 	int
   	Position pos.Position
+	Pokemons map[int]*NpcPokemon
+}
+
+type NpcPokemon struct {
+	Name 	string
+  	Hp int
+  	Att int
+  	Att_spec int
+  	Def int
+  	Def_spec int
+  	Speed int
+  	Gender int
+  	Held_item int
 }
 
 type NpcList struct {
@@ -38,9 +49,9 @@ func (m *NpcList) LoadNpcList() (succeed bool, error string) {
 	defer puh.DBFree()
 	for {
 		row := result.FetchRow()
-		if row == nil {
-			break
-		}
+			if row == nil {
+				break
+			}
 		
 		idNpc := puh.DBGetInt(row[0])
 		nameNpc := puh.DBGetString(row[1])		
@@ -57,21 +68,62 @@ func (m *NpcList) LoadNpcList() (succeed bool, error string) {
 	return true, ""
 }
 
+func (m *NpcList) LoadNpcPokemon() (succeed bool, error string) {
+		var query string = "SELECT idnpc, idnpc_pokemon, iv_hp, iv_attack, iv_attack_spec, iv_defence, iv_defence_spec, iv_speed, gender, held_item FROM npc_pokemon"
+		
+		result, err := puh.DBQuerySelect(query)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return false, err.Error()
+		}
+		
+		defer puh.DBFree()
+		for {
+		row := result.FetchRow()
+			if row == nil {
+				break
+			}
+			
+			idNpc := puh.DBGetInt(row[0])
+			idPokemon := puh.DBGetInt(row[1])
+			hp := puh.DBGetInt(row[2])
+			attack := puh.DBGetInt(row[3])		
+			attack_spec := puh.DBGetInt(row[4])
+			defence := puh.DBGetInt(row[5])
+			defence_spec := puh.DBGetInt(row[6])
+			speed := puh.DBGetInt(row[7])
+			gender := puh.DBGetInt(row[8])
+			held_item := puh.DBGetInt(row[9])
+			
+			//TODO Pokemon Names
+			m.Npcs[idNpc].AddPokemon(idPokemon, "Pokemon Name", hp, attack, attack_spec, defence, defence_spec, speed, gender, held_item)
+		}
+	return true, ""
+}
+
 func (m *NpcList) GetNumNpcs() int {
 	return len(m.Npcs)
 }
 
+func (m *NpcList) GetNumPokemons() int {
+	var count int
+	for _, npc := range(m.Npcs) {
+		count += len(npc.Pokemons)
+	}
+	return count
+}
+
 func (m *NpcList) AddNpc(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int, _position pos.Position) {
-	npc := &Npc { Id: _npcId,
-				  Name: _npcName,
+	npc := &Npc { Name: _npcName,
 				  Head: _head,
 				  Nek: _nek,
 				  Upper: _upper,
 				  Lower: _lower,
 				  Feet: _feet, 
-				  Position: _position }
-	
-	m.Npcs[_npcId] = npc
+				  Position: _position,
+				  Pokemons: make(map[int]*NpcPokemon) }
+			
+		m.Npcs[_npcId] = npc
 }
 
 func (m *NpcList) UpdateNpcAppearance(_npcId int, _npcName string, _head int, _nek int, _upper int, _lower int, _feet int) {
@@ -98,9 +150,19 @@ func (m *NpcList) UpdateNpcPosition(_npcId int, _position pos.Position) {
 }
 
 func (m *NpcList) DeleteNpc(_npcId int) {
-
-// Delete NPC
-delete(m.Npcs, _npcId)
-
+	// Delete NPC
+	delete(m.Npcs, _npcId)
 }
 
+func (m *Npc) AddPokemon(_id int, _name string, _hp int, _att int, _att_spec int, _def int, _def_spec int, _speed int, _gender int, _held_item int) {
+	npcPokemon := &NpcPokemon { 	Name : _name,
+									Hp : _hp,
+									Att : _att,
+									Att_spec : _att_spec,
+									Def : _def,
+									Def_spec : _def_spec,
+									Speed : _speed,
+									Gender : _gender,
+									Held_item : _held_item }
+	m.Pokemons[_id] = npcPokemon
+}
