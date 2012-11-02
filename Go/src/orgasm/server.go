fae1f73a5daf4b5c49afa280d6154ec6a9bcf86f
kortdanny@gmail.com <kortdanny@gmail.com@71effd0c-df8a-b9d7-22bd-8080055d6669>
2012-11-02 14:39:17 +0000

    [MapServer] Added chan for handling tile updates

--- Go/src/orgasm/server.go
+++ Go/src/orgasm/server.go
@@ -9,12 +9,14 @@ import (
 
 type Server struct {
 	port int
-	
 	clients map[int]*Client
+	tileChangeChan chan *Packet
 }
 
 func NewServer(_port int) *Server {
-	return &Server{port: _port, clients: make(map[int]*Client) }
+	return &Server{port: _port,
+		clients:        make(map[int]*Client),
+		tileChangeChan: make(chan *Packet)}
 }
 
 func (s *Server) RunServer() {
@@ -23,6 +25,8 @@ func (s *Server) RunServer() {
 		fmt.Printf("Server error: %v", err)
 		os.Exit(1)
 	}
+	
+	go s.HandleTileChange()
 	fmt.Printf("[Succeeded]\nWaiting for clients\n")
 
 	for {
@@ -32,7 +36,7 @@ func (s *Server) RunServer() {
 			break
 		}
 		
-		client := NewClient(clientsock)
+		client := NewClient(clientsock, s.tileChangeChan)
 		fmt.Printf("Client connected: %d\n", client.id)
 		
 		s.clients[client.id] = client
@@ -40,6 +44,99 @@ func (s *Server) RunServer() {
 		go client.HandleClient()
 	}
 	sock.Close()
+}
+
+func (s *Server) HandleTileChange() {
+	for {
+		packet := <-s.tileChangeChan
+		if packet == nil {
+			break
+		}
+
+		numTiles := int(packet.ReadUint16())
+		if numTiles <= 0 { // Zero tile selected bug
+			return
+		}
+
+		updatedTiles := list.New()
+
+		for i := 0; i < numTiles; i++ {
+			x := int(packet.ReadInt16())
+			y := int(packet.ReadInt16())
+			z := int(packet.ReadUint16())
+			blocking := int(packet.ReadUint16())
+			numLayers := int(packet.ReadUint16())
+
+			// Check if tile already exists
+			tile, exists := g_map.GetTileFromCoordinates(x, y, z)
+
+			if IS_DEBUG {
+				fmt.Printf("Tile Exists - %v - Layers: %d\n", exists, numLayers)
+			}
+
+			if numLayers > 0 {
+				if !exists { // Tile does not exists, create it		
+					if IS_DEBUG {
+						fmt.Printf("New Tile - X: %d - Y: %d - Z: %d\n", x, y, z)
+					}
+
+					tile = NewTileExt(x, y, z)
+				} else if IS_DEBUG {
+					fmt.Printf("Update Tile - X: %d - Y: %d - Z: %d - DbId: %d\n", x, y, z, tile.DbId)
+				}
+
+				// Set/update blocking
+				tile.SetBlocking(blocking)
+
+				for j := 0; j < numLayers; j++ {
+					layerId := int(packet.ReadUint16())
+					sprite := int(packet.ReadUint16())
+
+					tileLayer := tile.GetLayer(layerId)
+					if tileLayer == nil {
+						if IS_DEBUG {
+							fmt.Printf("Add Layer - Tile Id: %d - Layer: %d - DbId: %d\n", tile.DbId, layerId, tileLayer.DbId)
+						}
+
+						// Add and save new tile layer
+						tileLayer = tile.AddLayer(layerId, sprite)
+					} else {
+						if sprite == 0 {
+							if IS_DEBUG {
+								fmt.Printf("Delete Layer - Tile Id: %d - DbId: %d\n", tile.DbId, tileLayer.DbId)
+							}
+
+							// Remove layer, this will also remove the layer from database
+							tile.RemoveLayer(tileLayer)
+						} else {
+							if IS_DEBUG {
+								fmt.Printf("Update Layer - Tile Id: %d - DbId: %d\n", tile.DbId, tileLayer.DbId)
+							}
+
+							// Update tile layer with new sprite id
+							tileLayer.SetSpriteId(sprite)
+						}
+					}
+				}
+
+				// Save tile to database
+				tile.Save()
+			} else {
+				if exists {
+					if IS_DEBUG {
+						fmt.Printf("Delete Tile - Tile Id: %d\n", tile.DbId)
+					}
+
+					// Remove tile from database
+					tile.Delete()
+				}
+			}
+
+			updatedTiles.PushBack(tile)
+		}
+
+		s.SendTileUpdateToClients(updatedTiles, 0)
+	}
 }
 
 func (s *Server) SendTileUpdateToClients(_tiles *list.List, _sender int) {
